b72664b7b6ead460a1a8aa9f2660a037b5ced5a2
HaeJung1016 <57255189+HaeJung1016@users.noreply.github.com>
2020-07-27 15:25:16 +0900

    Update train.py

--- train/train.py
+++ train/train.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from PlaylistEmbedding import PlaylistEmbedding
 
-FILE_PATH = '../../dataset'
+FILE_PATH = '../dataset'
 U_space = PlaylistEmbedding(FILE_PATH)
 U_space.train_model()
 
